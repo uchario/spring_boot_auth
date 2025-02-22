0b431fee7ab5f0f1a6434ddcd0eff4453d7e376e
uchario <uchario@yahoo.com>
2025-02-22 23:01:02 +0100

    first commit

---
+++ section1/springsection1/src/main/java/com/eazybytes/springsection1/controller/WelcomeController.java
@@ -0,0 +1,12 @@
+package com.eazybytes.springsection1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @GetMapping("/welcome")
+    public String sayWelcome() {
+        return "Welcome to Spring Application with Security";
+    }
+}
