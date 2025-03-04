1632d3656f32e397d5439caab3e47698a9d11bca
uchario <uchario@yahoo.com>
2025-03-05 00:48:55 +0100

    Creating JPA entity and repository classes for new table

---
+++ section3/springsection3/src/main/java/com/eazybytes/springsection3/repository/CustomerRepository.java
@@ -0,0 +1,12 @@
+package com.eazybytes.springsection3.repository;
+
+import com.eazybytes.springsection3.model.Customer;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+    Optional<Customer> findByEmail(String email);
+}
